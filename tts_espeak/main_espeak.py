fb9e9029aa360fb5aafdf7d1084833f626d77afa
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2020-05-19 18:09:02 +0800

    change the folder to save data

--- tts_espeak/main_espeak.py
+++ tts_espeak/main_espeak.py
@@ -2,30 +2,30 @@ import os
 import time
 from datetime import datetime
 
-wav_folder = "data/tts_espeak/generated_speech/"
+BASE_FOLDER = "/home/mhilmiasyrofi/Documents/cross-asr/"
 
+wav_folder = "data/espeak/generated_speech/"
 if not os.path.exists(wav_folder):
     os.makedirs(wav_folder)
+
+exec_time_folder = "data/espeak/execution_time/"
+if not os.path.exists(exec_time_folder):
+    os.makedirs(exec_time_folder)
 
 file = open("corpus/europarl.txt")
 lines = file.readlines()
 file.close()
-
-exec_time_folder = "tts_espeak/execution_time/"
-if not os.path.exists(exec_time_folder):
-    os.makedirs(exec_time_folder)
 
 file = open(exec_time_folder + str(datetime.now()) + ".txt", "w+")
 
 i = 0
 for line in lines:
     i = i + 1
-    if (i >= 1 and i <= 1):
+    if (i >= 1 and i <= 20000):
         start_time = time.time()
         text = line[:-1]
-        outfile = "/home/mhilmiasyrofi/Documents/cross-asr/tts_espeak/temp_data/" + "audio_" + str(i) + ".wav"
-        wavfile = "/home/mhilmiasyrofi/Documents/cross-asr/" + \
-            wav_folder + "audio_" + str(i) + ".wav"
+        outfile = BASE_FOLDER + "tts_espeak/temp_data/" + "audio_" + str(i) + ".wav"
+        wavfile = BASE_FOLDER + wav_folder + "audio_" + str(i) + ".wav"
         cmd = "espeak \"" + text + "\" --stdout > " + outfile
         # print(cmd)
         os.system(cmd)
