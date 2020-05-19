eccd339d7067cd301bd091a4c4ac84a11bc1b53a
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2020-05-19 18:08:01 +0800

    adapt with dynamic TTS

--- create_training_data.py
+++ create_training_data.py
@@ -1,4 +1,7 @@
-import constant
+import constant, utils
+
+TTS = constant.TTS
+SR = constant.SR
 
 def get_bug_location(fpath) :
     file = open(fpath)
@@ -11,23 +14,20 @@ def get_bug_location(fpath) :
 
 def write_label(bugs, writer) :
     if (i in bugs):
-        writer.write(", " + str(constant.BUG_LABEL) +"\n")
+        writer.write(", " + str(constant.FAIL_TEST_CASE) +"\n")
     else :
-        writer.write(", " + str(constant.NON_BUG_LABEL) + "\n")
+        writer.write(", " + str(constant.SUCCESS_TEST_CASE) + "\n")
 
 def write_undetermined_test_case(writer) :
-    writer.write(", " + str(constant.UNDETERMINED_LABEL) + "\n")
+    writer.write(", " + str(constant.UNDETERMINED_TEST_CASE) + "\n")
 
 if __name__ == '__main__':
 
-    # alexa_bugs = get_bug_location("bug/sr/alexa_bug.txt")
-    # gcloud_bugs = get_bug_location("bug/sr/gcloud_bug.txt")
-    # gspeech_bugs = get_bug_location("bug/sr/gspeech_bug.txt")
-    deepspeech_bugs = get_bug_location("bug/sr/deepspeech_bug.txt")
-    wav2letter_bugs = get_bug_location("bug/sr/wav2letter_bug.txt")
-    wit_bugs = get_bug_location("bug/sr/wit_bug.txt")
-    paddledeepspeech_bugs = get_bug_location("bug/sr/paddledeepspeech_bug.txt")
-    undetermined_test_case_file = open("bug/undetermined_test_cases.txt")
+    bugs = {}
+    for sr in SR :
+        bugs[sr] = get_bug_location("data/" + TTS + "/" + sr + "/bug.txt")
+
+    undetermined_test_case_file = open("data/" + TTS + "/undetermined_test_cases.txt")
     lines = undetermined_test_case_file.readlines()
     undetermined_test_cases = []
     for id in lines :
@@ -35,54 +35,32 @@ if __name__ == '__main__':
     undetermined_test_case_file.close()
 
 
-    file = open("corpus-sentence.txt")
+    file = open(constant.CORPUS_FPATH)
     corpus = file.readlines()
     file.close() 
 
-    # alexa_training_data = open("training_data/alexa.txt", "w+")
-    # gcloud_training_data = open("training_data/gcloud.txt", "w+")
-    # gspeech_training_data = open("training_data/gspeech.txt", "w+")
-    deepspeech_training_data = open("training_data/deepspeech.txt", "w+")
-    wav2letter_training_data = open("training_data/wav2letter.txt", "w+")
-    wit_training_data = open("training_data/wit.txt", "w+")
-    paddledeepspeech_training_data = open("training_data/paddledeepspeech.txt", "w+")
-
+    training_data = {}
+    for sr in SR :
+        training_data[sr] = open("data/" + TTS + "/" + sr + "/training_data.txt", "w+")
 
     i = 0
     for sentence in corpus :
         i += 1
-        # alexa_training_data.write(sentence[:-1])
-        # gcloud_training_data.write(sentence[:-1])
-        # gspeech_training_data.write(sentence[:-1])
-        deepspeech_training_data.write(sentence[:-1])
-        wav2letter_training_data.write(sentence[:-1])
-        wit_training_data.write(sentence[:-1])
-        paddledeepspeech_training_data.write(sentence[:-1])
+
+        sentence = utils.preprocess_text(sentence[:-1])
+        
+        for sr in SR :
+            training_data[sr].write(sentence)
         
         if (i not in undetermined_test_cases) :
-            # write_label(alexa_bugs, alexa_training_data)
-            # write_label(gcloud_bugs, gcloud_training_data)
-            # write_label(gspeech_bugs, gspeech_training_data)
-            write_label(deepspeech_bugs, deepspeech_training_data)
-            write_label(wav2letter_bugs, wav2letter_training_data)
-            write_label(wit_bugs, wit_training_data)
-            write_label(paddledeepspeech_bugs, paddledeepspeech_training_data)
+            for sr in SR :
+                write_label(bugs[sr], training_data[sr])
         else :
-            # write_undetermined_test_case(alexa_training_data)
-            # write_undetermined_test_case(gcloud_training_data)
-            # write_undetermined_test_case(gspeech_training_data)
-            write_undetermined_test_case(deepspeech_training_data)
-            write_undetermined_test_case(wav2letter_training_data)
-            write_undetermined_test_case(wit_training_data)
-            write_undetermined_test_case(paddledeepspeech_training_data)
+            for sr in SR :
+                write_undetermined_test_case(training_data[sr])
 
 
-    # alexa_training_data.close()
-    # gcloud_training_data.close()
-    # gspeech_training_data.close()
-    deepspeech_training_data.close()
-    wav2letter_training_data.close()
-    wit_training_data.close()
-    paddledeepspeech_training_data.close()
-    
+    for sr in SR :
+        training_data[sr].close()
+
 
