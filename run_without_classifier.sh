d4a365f70242ad9c78309b88db6b1545ead49faf
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2020-09-12 10:33:32 +0800

    calculate average result from random experiments

--- run_without_classifier.sh
+++ run_without_classifier.sh
@@ -2,3 +2,4 @@ for i in 12346 12347 12348
 do
     python without_classifier.py --seed $i --number-of-batch 5 --batch-size 210 --batch-time 60
 done
+python calculate_averaged_result.py --approach without_classifier
